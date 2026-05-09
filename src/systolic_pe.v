a4e661474bee32a353cdae4c1af5df71f9e8429d
Ross0907 <140931356+Ross0907@users.noreply.github.com>
2026-05-10 03:34:23 +0530

    eadfdsfdfafafafsf

--- src/systolic_pe.v
+++ src/systolic_pe.v
@@ -37,7 +37,7 @@ module systolic_pe (
             b_out <= b_in;
 
             if (clear)
-                acc <= 6'd0;
+                acc <= product_ext;
             else
                 acc <= acc + product_ext;
         end
